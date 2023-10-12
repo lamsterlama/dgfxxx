ca47466f2a4b6e486f58f269c147738fc243a98e
lamsterlama <147688647+lamsterlama@users.noreply.github.com>
2023-10-12 11:35:09 +0530

    Add files via upload

---
+++ practical.1b.py
@@ -0,0 +1,14 @@
+#practical1.b
+#factorial of a number
+num = int(input("enter a number:"))
+factorial=1
+
+
+if num<0:
+    print("sorry, factorial for egative number does not exist")
+elif num==0:
+     print("the factoria of 0 is 1")
+else:
+  for i in range (1, num+1):
+   factorial = factorial * i
+print (" the factorial of: ",num,"is", factorial)
